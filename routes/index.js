5114d998659036e02fd8e7af43f5e06793ccaa7d
CJZ <65721288+Chan-JZ@users.noreply.github.com>
2022-04-01 03:55:06 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var counter = 0;
+router.get('/last.txt', function(req, res, next) {
+  if (counter ==0 ) {
+    res.send("");
+    counter++;
+  }
+  else {
+    counter++;
+    var time = new Date();
+    res.send(time);
+  }
+});
+
 module.exports = router;
